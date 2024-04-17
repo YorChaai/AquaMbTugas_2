c70560a4bb85f335e43974886894c3ef660d0bed
Diofavian RACH <diofavianrch@gmail.com>
2024-04-17 11:58:55 +0700

    perubahan total di hari senin 12:00

--- app/src/main/java/com/example/myapplication/Mainregister.java
+++ app/src/main/java/com/example/myapplication/Mainregister.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mainregister extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Mainregister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainregister);
+    }
+    public void mainhome3(View view) {
+        Intent intent = new Intent(Mainregister.this, Mainwebsite1.class);
+        startActivity(intent);
+    }
+    public void register(View view) {
+        Intent intent = new Intent(Mainregister.this, Mainhome2.class);
+        startActivity(intent);
     }
 }
